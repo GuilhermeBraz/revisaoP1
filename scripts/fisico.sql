b1ce91794bd3b46d264b044248a9ef821e858c8a
GuilhermeBraz <gvcerveirabraz@gmail.com>
2021-09-20 00:13:15 -0300

    change indent

--- scripts/fisico.sql
+++ scripts/fisico.sql
@@ -1,18 +1,20 @@
-/*--- - << Revisão P1 >> ------------------
- -- 
- --                    SCRIPT DE CRIACAO (DDL)
- -- 
- -- Data Criacao ...........: 18/09/2021
- -- Autor(es) ..............: Guilherme Verissimo Cerveira Braz
- -- Banco de Dados .........: MySQL
- -- Banco de Dados(nome) ...: revisaoP1
- -- 
- -- 
- -- - - PROJETO = > 01 Base de Dados - - = > 03 Tabelas -- -----------------------------------------------------------------
- */
+/*
+------------------ << Revisão P1 >> ------------------
+-- 
+--                    SCRIPT DE CRIACAO (DDL)
+-- 
+-- Data Criacao ...........: 18/09/2021
+-- Autor(es) ..............: Guilherme Verissimo Cerveira Braz
+-- Banco de Dados .........: MySQL
+-- Banco de Dados(nome) ...: revisaoP1
+-- 
+-- 
+-- - - PROJETO = > 01 Base de Dados 
+--             = > 03 Tabelas 
+*/ 
 CREATE DATABASE IF NOT EXISTS revisaoP1 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 USE revisaoP1;
---
+
 CREATE TABLE PESSOA (
     idPessoa INT NOT NULL AUTO_INCREMENT,
     nomeCompleto VARCHAR (100) NOT NULL,
@@ -27,7 +29,7 @@ CREATE TABLE PESSOA (
     CONSTRAINT PESSOA_FK PRIMARY KEY(idPessoa)
 ) Engine = InnoDB AUTO_INCREMENT = 1,
 DEFAULT CHARSET utf8;
---
+
 CREATE TABLE SINTOMA (
     idSintoma INT NOT NULL AUTO_INCREMENT,
     dataHora TIMESTAMP NOT NULL,
@@ -35,7 +37,7 @@ CREATE TABLE SINTOMA (
     CONSTRAINT SINTOMA_FK PRIMARY KEY(idSintoma)
 ) Engine = InnoDB AUTO_INCREMENT = 1,
 DEFAULT CHARSET utf8;
---
+
 CREATE TABLE tem (
     idPessoa INT NOT NULL,
     idSintoma INT NOT NULL,
